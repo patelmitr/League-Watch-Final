160171919121781bc4e0f52b327251804cac4ee8
Parth Patel <Parth@Pinas-MacBook-Pro.local>
2023-04-17 15:58:35 -0400

    Minor changes to init file

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -34,11 +34,13 @@ def create_app():
     from src.customers.customers import customers
     from src.products.products  import products
     from src.players.players import players
+    from src.leagueCommissioner.leagueCommissioner import leagueCommissioner
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
-    app.register_blueprint(players, url_prefix='/pl')
+    app.register_blueprint(players,     url_prefix='/pl')
+    app.register_blueprint(leagueCommissioner,     url_prefix='/l')
 
     return app
